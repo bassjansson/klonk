44044770e71e9a7efc5958632c135a8b29c0ad1c
Bass Jansson <tech@bassjansson.com>
2020-06-08 17:14:19 +0200

    Adds an mqtt node client test.

--- server/src/server/index.js
+++ server/src/server/index.js
@@ -1,12 +1,17 @@
+// Express
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const server = app.listen(port, () => console.log("Listening on port", port))
-const io = require('socket.io').listen(server)
-
-const arduinoEvents = require('./arduino-events.js')
 
 app.use(express.static('public'))
+
+
+// Socket.io
+
+const io = require('socket.io').listen(server)
+const arduinoEvents = require('./arduino-events.js')
 
 io.on('connect', socket => {
     console.log("New client connected!")
@@ -18,3 +23,23 @@ io.on('connect', socket => {
 
 arduinoEvents.on('pot-meter', value =>
     io.sockets.emit('msg', { pot: { value } }))
+
+
+// MQTT
+
+var mqtt = require('mqtt')
+var client = mqtt.connect('mqtt://localhost')
+
+client.on('connect', () => {
+    client.publish('testtopic', 'Hello mqtt!')
+
+    // client.subscribe('testtopic', err => {
+    //     if (!err) {}
+    // })
+})
+
+// client.on('message', (topic, message) => {
+//     // message is Buffer
+//     console.log(topic, message.toString())
+//     client.end()
+// })
